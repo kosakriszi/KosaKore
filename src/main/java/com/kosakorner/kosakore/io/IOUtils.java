be415a4548fc04152bf8479f8d41979c0b625164
kosakriszi <kosakriszi@gmail.com>
2014-11-27 21:54:35 -0500

    migrate more things over to this lib

---
+++ src/main/java/com/kosakorner/kosakore/io/IOUtils.java
@@ -0,0 +1,49 @@
+package com.kosakorner.kosakore.io;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IOUtils {
+
+    public static List<?> readArrayFromFile(File file) {
+        List<Object> array = new ArrayList<Object>();
+        if (!file.exists()) {
+            return array;
+        }
+        try {
+            FileReader reader = new FileReader(file);
+            BufferedReader buffer = new BufferedReader(reader);
+            String s;
+            while ((s = buffer.readLine()) != null) {
+                array.add(s);
+            }
+            reader.close();
+            return array;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return array;
+    }
+
+    public static void writeArrayToFile(List<?> array, File file) {
+        StringBuilder contents = new StringBuilder();
+        for (Object object : array) {
+            contents.append(object).append("\n");
+        }
+        try {
+            FileWriter writer = new FileWriter(file);
+            writer.write(contents.toString());
+            writer.flush();
+            writer.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
