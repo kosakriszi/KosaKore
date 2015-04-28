c6add92cb8aedccfd141fcca33ec11722f9a91e8
kosakriszi <kosakriszi@gmail.com>
2015-04-27 20:47:39 -0400

    CraftBukkit version independant

--- Kore-Bukkit/src/main/java/com/kosakorner/kosakore/bukkit/command/BukkitDispatcherFactory.java
+++ Kore-Bukkit/src/main/java/com/kosakorner/kosakore/bukkit/command/BukkitDispatcherFactory.java
@@ -6,22 +6,35 @@ import com.kosakorner.kosakore.bukkit.Kore;
 import org.bukkit.Bukkit;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.command.SimpleCommandMap;
-import org.bukkit.craftbukkit.v1_8_R2.CraftServer;
 import org.bukkit.plugin.Plugin;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
 public class BukkitDispatcherFactory implements IDispatcherFactory {
+
+    private static final String[] craftServerClasses = new String[]{"org.bukkit.craftbukkit.v1_8_R2.CraftServer", "org.bukkit.craftbukkit.v1_8_R1.CraftServer"};
 
     SimpleCommandMap commandMap;
 
     public BukkitDispatcherFactory() {
-        try {
-            commandMap = ((CraftServer) Bukkit.getServer()).getCommandMap();
+        Class<?> craftServerClass = null;
+        for (String className : craftServerClasses) {
+            try {
+                craftServerClass = Class.forName(className);
+                if (craftServerClass != null) {
+                    Method getCommandMap = craftServerClass.getMethod("getCommandMap");
+                    commandMap = (SimpleCommandMap) getCommandMap.invoke(Bukkit.getServer());
+                }
+                break;
+            }
+            catch (Exception e) {
+
+            }
         }
-        catch (Exception e) {
-            Kore.log("FATAL - Unable to hook into Bukkit command map!");
-            e.printStackTrace();
+
+        if (craftServerClass == null) {
+            Kore.log("FATAL - Unable to hook into the Bukkit command map! There WILL BE ISSUES");
         }
     }
 
